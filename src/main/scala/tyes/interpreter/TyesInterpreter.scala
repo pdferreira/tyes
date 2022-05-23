6790af02a13c6f29f6a127bd49bc12ad2cfd4196
Paulo Ferreira <gammalambda333@gmail.com>
2022-05-24 00:16:47 +0100

    Added tyer support for single-var environments

--- src/main/scala/tyes/interpreter/TyesInterpreter.scala
+++ src/main/scala/tyes/interpreter/TyesInterpreter.scala
@@ -1,47 +1,55 @@
 package tyes.interpreter
 
 import tyes.model.*
+import TyesLanguageExtensions.*
 
 object TyesInterpreter:
-  
-  def typecheck[E](tsDecl: TypeSystemDecl, ruleDecl: RuleDecl, term: Term): Option[Type] = ruleDecl.conclusion match {
-    case Judgement(_, HasType(metaTerm, typ)) => metaTerm.matches(term).flatMap { subst => 
-      // check all premises hold, while unifying possible type variables
-      val (premisesHold, typeVarEnv) = ruleDecl.premises.foldLeft((true, Map[String, Type.Named]())) { 
-        // if one of the premises failed, propagate failure
-        case (acc @ (false, typeVarEnv), _) => acc 
-        // otherwise, check the next premise
-        case ((true, typeVarEnv), Judgement(_, HasType(premTerm, premTyp))) => 
-          val resTyp = typecheck(tsDecl, premTerm.substitute(subst))
-          premTyp match {
-            // if we expect a named type, just compare directly
-            case t @ Type.Named(_) => (resTyp == Some(t), typeVarEnv)
-            // otherwise, check if we already know what to match the type variable against
-            // and if we don't then associate the premise result type
-            case Type.Variable(name) =>
-              if typeVarEnv.contains(name) 
-              then (resTyp == Some(typeVarEnv(name)), typeVarEnv)
-              else resTyp match {
-                case Some(t @ Type.Named(_)) => (true, typeVarEnv + (name -> t))
-                case _ => (false, typeVarEnv)
-              }
-          }
-      }
 
-      if premisesHold then
-        Some(typ match {
-          case Type.Named(_) => typ
-          case Type.Variable(name) => typeVarEnv(name)
-        })
-      else
-        None
-    }
+  def typecheck(tsDecl: TypeSystemDecl, ruleDecl: RuleDecl, term: Term, termEnv: Map[String, Type.Named]): Option[Type] = ruleDecl.conclusion match {
+    case Judgement(metaEnvOpt, HasType(metaTerm, typ)) =>
+      // match the conclusion env to the rule to see if it's applicable at all
+      val typSubstOpt = metaEnvOpt match {
+        case None => Some(Map())
+        case Some(metaEnv) => metaEnv.matches(termEnv)
+      }
+      
+      typSubstOpt.zip(metaTerm.matches(term)).flatMap { (typeSubst, subst) =>
+        // check all premises hold, while unifying possible type variables
+        val (premisesHold, finalTypeVarEnv) = ruleDecl.premises.foldLeft((true, typeSubst)) { 
+          // if one of the premises failed, propagate failure
+          case (acc @ (false, typeVarEnv), _) => acc 
+          // otherwise, check the next premise
+          case ((true, typeVarEnv), Judgement(premMetaEnvOpt, HasType(premTerm, premTyp))) => 
+            // replace the type variables we already know in the premise env and then produce an
+            // actual term env out of it
+            val premEnvOpt = premMetaEnvOpt.map(_.substitute(typeVarEnv).toConcrete).getOrElse(Some(Map()))
+            val resTyp = premEnvOpt.flatMap(premEnv => typecheck(tsDecl, premTerm.substitute(subst), premEnv))
+            premTyp match {
+              // if we expect a named type, just compare directly
+              case t @ Type.Named(_) => (resTyp == Some(t), typeVarEnv)
+              // otherwise, check if we already know what to match the type variable against
+              // and if we don't then associate the premise result type
+              case Type.Variable(name) =>
+                if typeVarEnv.contains(name) 
+                then (resTyp == Some(typeVarEnv(name)), typeVarEnv)
+                else resTyp match {
+                  case Some(t @ Type.Named(_)) => (true, typeVarEnv + (name -> t))
+                  case _ => (false, typeVarEnv)
+                }
+            }
+        }
+
+        if premisesHold then
+          Some(typ.substitute(finalTypeVarEnv))
+        else
+          None
+      }
   }
 
-  def typecheck(tsDecl: TypeSystemDecl, term: Term): Option[Type] =
+  def typecheck(tsDecl: TypeSystemDecl, term: Term, env: Map[String, Type.Named]): Option[Type] =
     tsDecl
       .rules
-      .collectFirst(Function.unlift(r => typecheck(tsDecl, r, term)))
+      .collectFirst(Function.unlift(r => typecheck(tsDecl, r, term, env)))
   
   def typecheck[E](tsDecl: TypeSystemDecl, exp: E)(using Conversion[E, Term]): Option[Type] =
-    typecheck(tsDecl, exp.convert)
+    typecheck(tsDecl, exp.convert, Map())
---
+++ src/main/scala/tyes/interpreter/TyesLanguageExtensions.scala
@@ -0,0 +1,39 @@
+package tyes.interpreter
+
+import tyes.model.*
+
+object TyesLanguageExtensions:
+  
+  extension (metaEnv: Environment)
+
+    def matches(env: Map[String, Type.Named]): Option[Map[String, Type.Named]] = metaEnv match {
+      case Environment.BindName(name, typ) =>
+        if env.contains(name) && env.size == 1 then
+          typ match {
+            case t @ Type.Named(_) => 
+              if t == env(name) then
+                Some(Map())
+              else
+                None
+            case Type.Variable(typVarName) =>
+              Some(Map(typVarName -> env(name)))
+          }
+        else
+          None
+    }
+
+    def substitute(typeVarEnv: Map[String, Type.Named]): Environment = metaEnv match {
+      case Environment.BindName(name, typ) => Environment.BindName(name, typ.substitute(typeVarEnv))
+    }
+
+    def toConcrete: Option[Map[String, Type.Named]] = metaEnv match {
+      case Environment.BindName(name, typ @ Type.Named(_)) => Some(Map(name -> typ))
+      case _ => None
+    }
+
+  extension (typ: Type)
+
+    def substitute(typeVarEnv: Map[String, Type.Named]): Type = typ match {
+      case Type.Named(_) => typ
+      case Type.Variable(name) => typeVarEnv(name)
+    }
