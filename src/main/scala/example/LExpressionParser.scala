adbf550a25b2d4bd90ae58f535d3a2351a705db6
Paulo Ferreira <gammalambda333@gmail.com>
2022-12-14 01:29:20 +0000

    Added support for lambda and app in example language

--- src/main/scala/example/LExpressionParser.scala
+++ src/main/scala/example/LExpressionParser.scala
@@ -4,7 +4,9 @@ import scala.util.parsing.combinator.RegexParsers
 
 class LExpressionParser[TType] extends RegexParsers:
 
-  def ident = raw"[a-zA-Z][a-zA-Z\d_]*".r
+  val keywords = Set("let", "in", "fun")
+
+  def ident = raw"[a-zA-Z][a-zA-Z\d_]*".r.filter(id => !keywords.contains(id))
 
   def variable = ident ^^ { varName => LVariable(varName) } 
 
@@ -12,16 +14,24 @@ class LExpressionParser[TType] extends RegexParsers:
   
   def leaf = ("(" ~> expression <~ ")") | number | variable 
 
-  def operator = leaf ~ ("+" ~> leaf).* ^^ { 
+  def app = leaf ~ leaf.* ^^ {
+    case exp ~ rs => rs.foldLeft(exp) { (left, right) => LApp(left, right) }
+  }
+
+  def operator = app ~ ("+" ~> app).* ^^ { 
     case exp ~ rs => rs.foldLeft(exp) { (left, right) => LPlus(left, right) }
   }
 
   def tpe: Parser[TType] = failure("No parser for types defined")
 
-  def let = ("let" ~> ident) ~ (":" ~> tpe).? ~ ("=" ~> operator) ~ ("in" ~> expression) ^^ {
+  def let: Parser[LExpression[TType]] = ("let" ~> ident) ~ (":" ~> tpe).? ~ ("=" ~> (expression - let)) ~ ("in" ~> expression) ^^ {
     case varName ~ varTypeOpt ~ varExp ~ inExp => LLet(varName, varTypeOpt, varExp, inExp)
   }
-  
-  def expression: Parser[LExpression[TType]] = let | operator
+
+  def fun: Parser[LExpression[TType]] = ("fun" ~> ident) ~ (":" ~> tpe).? ~ ("->" ~> expression) ^^ {
+    case argName ~ argTypeOpt ~ bodyExp => LFun(argName, argTypeOpt, bodyExp)
+  }
+
+  def expression: Parser[LExpression[TType]] = let | fun | operator
 
   def parse(input: String) = super.parse(phrase(expression), input)
